a3158e68cb6b199bd3dfe9907c8e0cc59ef0ebda
Ecboxer <ecboxer@gmail.com>
2018-10-10 21:01:42 -0400

    Wrote cdf histogram ink

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Inkrement
 
 Inkrement provides tools for incremental data analysis. You might find it most useful when the questions you have can be answered from a subset of your data. The tools may also be useful in determining if a partial view of your data can be used to draw conclusions, ie are differences in your data by orders of magnitude or is your data fairly homogeneous.  
 
-Typical usage often looks like this:
+Typical scatterplot usage looks like this:
 
     #!usr/bin/env python
 	
@@ -14,12 +14,28 @@ Typical usage often looks like this:
 	x = np.random.normal(0, 1, 1000)
 	y = np.random.normal(0, 1, 1000)
 	
-	inkrement.plot_lag(x, y, 10, 53, 8)
+	inkrement.plot_lag(x, y, n = 10, group = 53, lag = 8)
 	
 Output looks like this:  
 
 ![Example Plot](https://github.com/Ecboxer/inkrement/blob/master/docs/testplot.png)
+
+Cumulative Histogram usage looks like:
+
+	#!usr/bin/env python
 	
+	import inkrement
+	
+	import numpy as np
+	
+	x = np.random.normal(0, 1, 1000)
+	
+	inkrement.inc_hist_cdf(x, n = 25, start = 167, inc = 8, bins = 15)
+	
+Output looks like this:  
+
+![Example Plot](https://github.com/Ecboxer/inkrement/blob/master/docs/histcdf.png)
+
 Summary
 =======
 
--- inkrement/__init__.py
+++ inkrement/__init__.py
@@ -1,7 +1,24 @@
 name = 'inkrement'
 
 def plot_lag(x, y, n, group = 0, lag = 0):
-    """Scatter plot of group-th n elements of data and lag * n elements previous"""
+    """Scatter plot of group-th n elements of data and lag * n elements previous
+    
+    Parameters
+    ----------
+    x : independent variable
+    
+    y : dependent variable
+    
+    n : number of elements to plot in each batch
+    
+    group : index of final batch of data
+    
+    lag : number of batches before group-th to be plotted
+    
+    Output
+    ------
+    Scatterplot with labelled legend
+    """
     import matplotlib.pyplot
     import matplotlib.patches
 
@@ -15,7 +32,7 @@ def plot_lag(x, y, n, group = 0, lag = 0):
              '#08519c',
              '#08306b']
     
-    l = lag
+    l = int(lag)
     if l < 0:
         print('lag **kwarg should be an int between 0 and 8')
         raise
@@ -31,9 +48,63 @@ def plot_lag(x, y, n, group = 0, lag = 0):
     for i in range(l): #Plot lagged groups in lighter color
         data_x = x[(group - i - 1) * n:(group - i) * n]
         data_y = y[(group - i - 1) * n:(group - i) * n]
-        axs.scatter(data_x, data_y, color=blues[-1-i])
+        axs.scatter(data_x, data_y, color=blues[-2-i])
         
     patches = []
     for i in range(l + 1): #Create legend patches
         patches.append(matplotlib.patches.Patch(color=blues[-1-i], label='{0} [{1}:{2}], {3} [{1}:{2}]'.format('x', (group - i) * n, (group - i + 1) * n, 'y')))
+    matplotlib.pyplot.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0)
+
+def inc_hist_cdf(x, n, start = 0, inc = 0, bins = 20):
+    """
+    Plots cumulative histograms of the group-th n * (i + 1) elements of x for 0 <= i <= lag
+    
+    Parameters
+    ----------
+    x : data
+    
+    n : number of observations to include in the first batch and to increase by in each subsequent batch
+    
+    start : start index of first batch of data
+    
+    inc : number of batches to be plotted
+    
+    bins : number of bins for each cumulative histogram
+    
+    Output
+    ------
+    Overayed histograms for (inc + 1) * n observations beginning at start
+    """
+    import matplotlib.pyplot
+    import matplotlib.patches
+
+    blues = ['#f7fbff',
+             '#deebf7',
+             '#c6dbef',
+             '#9ecae1',
+             '#6baed6',
+             '#4292c6',
+             '#2171b5',
+             '#08519c',
+             '#08306b']
+    
+    inc = int(inc)
+    if inc < 0:
+        print('inc **kwarg should be an int between 0 and 8')
+        raise
+    if inc > 8:
+        print('max recommended lag **kwarg is 8')
+        inc = 8
+    
+    fig, axs = matplotlib.pyplot.subplots()
+    axs.hist(x[start:(start + n)], bins, histtype = 'step', cumulative = True, density = True, color = blues[-(inc + 1)])
+    axs.set_xlabel('x')
+    axs.set_ylabel('density')
+    for i in range(inc):
+        data_x = x[start:(start + n * (i + 2))]
+        axs.hist(data_x, bins, histtype = 'step', cumulative = True, density = True, color = blues[-inc + i])
+        
+    patches = []
+    for i in range(inc + 1): #Create legend patches
+        patches.append(matplotlib.patches.Patch(color=blues[-(inc + 1) + i], label='{0} [{1}:{2}]'.format('x', start, start + n * (i + 1))))
     matplotlib.pyplot.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0)
